4d7cdcdb9fd8aa6e39650354e49963232c443361
SAMOVENDYUK NIKOLAY <samovnick@gmail.com>
2019-03-12 21:12:33 +0300

    Complete advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  // your implementation+
+  // your solution
+
+  let primeNumbers = new Set();
+  let K = base;
+  let i = 2;
+  while (K > 1) {
+    if (K % i == 0) {
+      primeNumbers.add(i);
+      K = Math.floor(K / i);
+    } else {
+      i++;
+    }
+  }
+
+  //create array a, b
+
+  let a = [];
+  let b = [];
+
+  for (let tmp of primeNumbers) {
+    K = base;
+    i = 0;
+    while (!(K % tmp)) {
+      K = Math.floor(K / tmp);
+      i++;
+    }
+    a.push(i);
+
+    K = number;
+    i = 0;
+    while (K >= 1) {
+      K /= tmp;
+      i += Math.floor(K);
+    }
+    b.push(i);
+  }
+
+  //min
+  let min = Math.floor(b[0] / a[0]);
+  for (let i = 1; i < a.length; i++) {
+    console.log(min);
+    min = Math.min(min, Math.floor(b[i] / a[i]));
+  }
+
+  return min;
+};
